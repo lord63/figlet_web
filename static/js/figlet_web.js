4b2ddd49755bb921187918ff3194b0067566f83f
lord63 <lord63.j@gmail.com>
2015-03-27 16:07:57 +0800

    Generate the text when user stop typing, #1
    still can't generate text when user change fonts or layout options

---
+++ static/js/figlet_web.js
@@ -0,0 +1,49 @@
+// Generate the text use input.
+function generate() {
+  figlet($('input').val(), {
+    font: $('#font').val(),
+    horizontalLayout: $('#horizontalLayout').val(),
+    verticalLayout: $('#verticalLayout').val()
+  }, function(err, data) {
+       if (err) {
+       console.log('Something went wrong...');
+       console.dir(err);
+       return;
+     }
+     $('pre').html(data);
+  });
+}
+
+
+$('#generate').on('click', function() {
+  generate();
+});
+
+
+// Generate the text when user stop typing.
+// http://blog.another-d-mention.ro/programming/java-script/on-typing-complete-jquery-plugin/
+$.fn.onTypeFinished = function (func) {
+  var T, S = 0, D = 1000;
+  $(this).bind("keypress", onKeyPress).bind("focusout", onTimeOut);
+  function onKeyPress() {
+    clearTimeout(T);
+    if (S === 0) {
+      S = new Date().getTime();
+      D = 1000;
+      T = setTimeout(onTimeOut, 1000);
+      return;
+    }
+    var t = new Date().getTime();
+    D = (D + (t - S)) / 2;
+    S = t;
+    T = setTimeout(onTimeOut, D * 2);
+  }
+  function onTimeOut() {
+    func.apply();
+    S = 0;
+  }
+  return this;
+};
+
+
+$("input[name='input_text']").onTypeFinished(generate);
